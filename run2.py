0c87fe067380962b3b6afdf1c3873df959987664
tejcamilo <ctejada@twilio.com>
2024-06-17 16:11:35 -0500

    Check if there are active conversations

--- run2.py
+++ run2.py
@@ -10,22 +10,33 @@ auth_token = os.getenv('AUTH_TOKEN')
 workspace_sid = os.getenv('WORKSPACE_SID')
 client = Client(account_sid, auth_token)
 
-conversations = [] # List of all active conversations
-# Fetch all active conversations
 
-for conversation in client.conversations.v1.conversations.list():
-    if(conversation.state == 'active'):
-        conversations.append(conversation)
+def run():
+    conversations = [] # List of all active conversations
+    # Fetch all active conversations
 
-task_conversation_sids = [] # Get list of tasks with conversation SIDs
-# Create a set of conversation SIDs that have associated tasks
-for task in client.taskrouter.v1.workspaces(workspace_sid).tasks.list():
-    json_obj = json.loads(task.attributes)
-    if 'conversationSid' in json_obj:
-        task_conversation_sids.append(json_obj["conversationSid"])
+    for conversation in client.conversations.v1.conversations.list():
+        if(conversation.state == 'active'):
+            conversations.append(conversation)
 
-# Filter conversations that don't have associated tasks
-conversations_without_tasks = [conversation for conversation in conversations if conversation.sid not in task_conversation_sids]
+    task_conversation_sids = [] # Get list of tasks with conversation SIDs
+    # Create a set of conversation SIDs that have associated tasks
+    for task in client.taskrouter.v1.workspaces(workspace_sid).tasks.list():
+        json_obj = json.loads(task.attributes)
+        if 'conversationSid' in json_obj:
+            task_conversation_sids.append(json_obj["conversationSid"])
 
-for conversation in conversations_without_tasks:
-    print(f"Conversations without task: {conversation.sid}")
+    # Filter conversations that don't have associated tasks
+    if len(conversations) > 0:
+        conversations_without_tasks = [conversation for conversation in conversations if conversation.sid not in task_conversation_sids]
+
+        for conversation in conversations_without_tasks:
+            print(f"Conversations without task: {conversation.sid}")
+    else:
+        print("No active conversations found")
+        return
+
+
+
+if __name__ == "__main__":
+    run() 
